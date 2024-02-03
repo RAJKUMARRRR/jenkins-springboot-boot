efedcf4c88a504f291207cd01310e94f2cecde1f
Rajkumar Chinthala <smilingrajkumar72@gmail.com>
2024-02-03 20:32:06 +0000

    added rest api

---
+++ src/main/java/com/practice/ec2jenkins/DemoController.java
@@ -0,0 +1,13 @@
+package com.practice.ec2jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Congratulations, application deployed";
+    }
+}
--- src/test/java/com/practice/ec2jenkins/Ec2JenkinsApplicationTests.java
+++ src/test/java/com/practice/ec2jenkins/Ec2JenkinsApplicationTests.java
@@ -1,13 +1,19 @@
 package com.practice.ec2jenkins;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class Ec2JenkinsApplicationTests {
 
+	@Autowired
+	DemoController demoController;
 	@Test
-	void contextLoads() {
+	void successTest() {
+		assertEquals("Congratulations, application deployed",demoController.hello());
 	}
 
 }
