b9de11ab2729779817ca48aaae8e862484760730
Rajkumar Chinthala <smilingrajkumar72@gmail.com>
2024-02-03 20:19:18 +0000

    init
    init

---
+++ src/main/java/com/practice/ec2jenkins/Ec2JenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.practice.ec2jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2JenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec2JenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/ec2jenkins/Ec2JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.ec2jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2JenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
